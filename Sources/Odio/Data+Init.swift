13d2678923dfdbc50547a2debcd006470f57d9ab
Barreloofy <172546110+Barreloofy@users.noreply.github.com>
2026-01-23 15:39:18 +0100

    Remove obsolete Data initializer

--- Sources/Odio/Data+Init.swift
+++ Sources/Odio/Data+Init.swift
@@ -8,25 +8,6 @@
 import Foundation
 
 extension Data {
-	/// - Parameters:
-	///   - name: The name of the audio file.
-	///   - bundle: The bundle to retrieve the file from.
-	init(
-		name: String,
-		bundle: Bundle = .main)
-	throws {
-		do {
-			guard
-				let url = bundle.url(forResource: name, withExtension: "")
-			else { throw OdioError(errorDescription: "File could not be located.") }
-
-			self = try Data(contentsOf: url)
-		} catch {
-			errorLogger.error("\(error.localizedDescription)")
-			throw error
-		}
-	}
-
 	init(
 		forResource name: String,
 		withExtension `extension`: String? = nil,
@@ -40,25 +21,6 @@ extension Data {
 					withExtension: `extension`,
 					subdirectory: subpath)
 			else { throw OdioError(errorDescription: "Resource not found: \(name).\(`extension` ?? "")") }
-
-			self = try Data(contentsOf: url)
-		} catch {
-			errorLogger.error("\(error.localizedDescription)")
-			throw error
-		}
-	}
-
-	/// - Parameters:
-	///   - keyPath: A key path to a specific resulting value representing an audio file.
-	///   - bundle: The bundle to retrieve the file from.
-	init(
-		keyPath: KeyPath<FileKey, String>,
-		bundle: Bundle = .main)
-	throws {
-		do {
-			guard
-				let url = bundle.url(forResource: FileKey()[keyPath: keyPath], withExtension: "")
-			else { throw OdioError(errorDescription: "File could not be located.") }
 
 			self = try Data(contentsOf: url)
 		} catch {
